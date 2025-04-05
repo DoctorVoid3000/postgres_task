8c3e0b45ebae6c0e35379c76700a7694880318a7
DoctorVoid3000 <eugene.arlyapov@yandex.ru>
2025-04-05 22:33:33 +0300

    Added queries and execution plans

---
+++ sql/execution_plan.sql
@@ -0,0 +1,46 @@
+--Q1
+\c pg1
+
+EXPLAIN SELECT COUNT(P.tags) AS CountTags, P.tags, AVG(C.CreationDate - P.CreationDate) AS AvgAnswerTime, AVG(P.Score)::INTEGER AvgScore
+FROM posts.posts AS P JOIN posts.Comments AS C ON P.Id = C.PostId
+WHERE tags LIKE '%|postgresql|%' AND LENGTH(tags) - LENGTH(REPLACE(tags, '|', '')) = 3 
+GROUP BY tags
+ORDER BY CountTags DESC
+LIMIT 5;
+
+EXPLAIN ANALYZE SELECT COUNT(P.tags) AS CountTags, P.tags, AVG(C.CreationDate - P.CreationDate) AS AvgAnswerTime, AVG(P.Score)::INTEGER AvgScore
+FROM posts.posts AS P JOIN posts.Comments AS C ON P.Id = C.PostId
+WHERE tags LIKE '%|postgresql|%' AND LENGTH(tags) - LENGTH(REPLACE(tags, '|', '')) = 3 
+GROUP BY tags
+ORDER BY CountTags DESC
+LIMIT 5;
+
+CREATE INDEX index_tags ON posts.posts(tags);
+
+EXPLAIN SELECT COUNT(P.tags) AS CountTags, P.tags, AVG(C.CreationDate - P.CreationDate) AS AvgAnswerTime, AVG(P.Score)::INTEGER AvgScore
+FROM posts.posts AS P JOIN posts.Comments AS C ON P.Id = C.PostId
+WHERE tags LIKE '%|postgresql|%' AND LENGTH(tags) - LENGTH(REPLACE(tags, '|', '')) = 3 
+GROUP BY tags
+ORDER BY CountTags DESC
+LIMIT 5;
+
+EXPLAIN ANALYZE SELECT COUNT(P.tags) AS CountTags, P.tags, AVG(C.CreationDate - P.CreationDate) AS AvgAnswerTime, AVG(P.Score)::INTEGER AvgScore
+FROM posts.posts AS P JOIN posts.Comments AS C ON P.Id = C.PostId
+WHERE tags LIKE '%|postgresql|%' AND LENGTH(tags) - LENGTH(REPLACE(tags, '|', '')) = 3 
+GROUP BY tags
+ORDER BY CountTags DESC
+LIMIT 5;
+
+DROP INDEX Posts.index_tags;
+
+--Q2
+-- SELECT DisplayName AS dname, Score FROM Users.Users U
+-- JOIN Posts.Posts P ON U.id = P.OwnerUserId
+-- WHERE AcceptedAnswerId IS NOT NULL 
+-- AND Score IN (SELECT Score FROM posts.posts
+--               WHERE AcceptedAnswerId IS NOT NULL 
+--               GROUP BY Score
+--               HAVING Score < 0) 
+-- AND Tags LIKE '%|postgresql|%'
+-- ORDER BY Score
+-- LIMIT 5;
